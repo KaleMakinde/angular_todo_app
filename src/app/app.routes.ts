4095823d16f5647389bb5e438a62760a275032cf
KaleMakinde <makindeadeyemiadekale@gmail.com>
2024-07-24 21:29:22 +0100

    initial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: TodosComponent}
+];
+
+
+@NgModule ({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
